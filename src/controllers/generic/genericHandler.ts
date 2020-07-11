f4b775c37764fd3955265952a5bfb82abf4aa99e
Baraglia <rodrigo.d.baraglia@accenture.com>
2020-07-11 17:47:21 -0300

    more refactoring

--- src/controllers/generic/genericHandler.ts
+++ src/controllers/generic/genericHandler.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
 import { respond } from "../../services/respond";
 
-export function handleRequest<F extends Function,
-  G extends Function,
-  H extends Function>(procedure: F) {
-  return (model: G, message: H) => async (
-    req: Request,
-    res: Response
-  ): Promise<Response> => respond(res, message({ data: await procedure(model, req) }));
+export function handleRequest(procedure: any) {
+  return function _handleRequest(model: any, message: any) {
+    return async function __handleRequest(
+      req: Request,
+      res: Response
+    ): Promise<Response> {
+      return respond(res, message({ data: await procedure(model, req) }));
+    };
+  };
 }
