0d1b984104e49f0f37d559e041354c3deb22e54b
Baraglia <rodrigo.d.baraglia@accenture.com>
2020-07-14 12:32:31 -0300

    refactored type declarations

--- src/utils/declarations.d.ts
+++ src/utils/declarations.d.ts
@@ -1,14 +1,13 @@
-import {Last} from "typescript-tuple"
-import {Head, Composibility} from "./utilities"
+import { Last } from "typescript-tuple";
+import { Head, Composibility } from "./utilities";
 
 export interface IAsync {
   <T extends any[], Z>(...args: T): Promise<Z>;
 }
 
+export type Composable<T> = Composibility<T> extends true ? T : never;
 
-export type Composable<T> = Composibility<T> extends true ? T : never
-
-//Typescript-tuple provides tuple manipulation methods that simplify working with multiple generic types. 
+//Typescript-tuple provides tuple manipulation methods that simplify working with multiple generic types.
 export interface IComposite<T extends any[]> {
   <U extends Parameters<Last<T>>, S extends ReturnType<Head<T>>>(...args: U): S;
 }
@@ -16,4 +15,3 @@ export interface IComposite<T extends any[]> {
 export interface IPipeline<T> {
   <U extends Parameters<Head<T>>, S extends ReturnType<Last<T>>>(...args: U): S;
 }
-
