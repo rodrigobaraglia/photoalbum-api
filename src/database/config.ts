a0ebca4fcdd9d33217570b4142db836a4eb76678
Baraglia <rodrigo.d.baraglia@accenture.com>
2020-07-17 14:41:33 -0300

    more refactoring

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,13 +1,10 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-  const password = "THSoQto8f140g8LG";
-  const dbname = "galleri-api";
-  const uri = `mongodb+srv://rodrigobaraglia:${password}@cluster0-6mmbk.mongodb.net/${dbname}?retryWrites=true&w=majority`;
   const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   };
-  await mongoose.connect(uri, options);
+  await mongoose.connect(String(process.env.CONNECTION_STRING), options);
   console.log("Database is connected");
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { app, PORT } from "./app";
 import {connect} from "./database";
 import {composePipeTestCase} from "./utils/testCase"
+import * as dotenv from 'dotenv';
 
 async function main() {
+  dotenv.config()
   await connect();
   await app.listen(PORT);
   console.log(`Server running on ${PORT}`);
-
+  // config()
 }
+
 main();
 composePipeTestCase();
 
